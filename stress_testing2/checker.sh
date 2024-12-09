5789864ec2bc0cb2d289b457e7b11da5c9464bc0
Md. Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-12-09 14:54:48 +0600

    Update checker.sh

--- stress_testing2/checker.sh
+++ stress_testing2/checker.sh
@@ -16,3 +16,23 @@ echo "Your answer is:"
 cat myOutput
 echo "Correct answer is:"
 cat bruteOutput
+
+
+# or you can use
+# set -e
+#g++ code.cpp -o code
+#g++ gen.cpp -o gen
+#g++ brute.cpp -o brute
+#for((i = 1; ; ++i)); do
+#    ./gen $i > input_file
+#    ./code < input_file > myAnswer
+#    ./brute < input_file > correctAnswer
+#    diff -Z myAnswer correctAnswer > /dev/null || break
+#    echo "Passed test: "  $i
+#done
+#echo "WA on the following test:"
+#cat input_file
+#echo "Your answer is:"
+#cat myAnswer
+#echo "Correct answer is:"
+#cat correctAnswer
