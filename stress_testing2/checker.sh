c98f285689aaf9f1428a622519173a064495bab9
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-12-09 14:13:39 +0600

    erricto stress testing learning

--- stress_testing2/checker.sh
+++ stress_testing2/checker.sh
@@ -2,7 +2,7 @@ g++ my_solution.cpp -o my_solution
 g++ bruteForce_solution.cpp -o bruteForce_solution
 g++ randomTestGenerator.cpp -o randomTestGenerator
 
-for((i = 1; ; ++i)); do
+for((i = 1; i<100; ++i)); do
     ./randomTestGenerator $i > randomInput
     ./my_solution <randomInput > myOutput
     ./bruteForce_solution <randomInput > bruteOutput
